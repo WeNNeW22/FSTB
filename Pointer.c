67b2ab0edd549e7814ffb10a8a653804b7bb6a39
WeNNeW22 <138551212+WeNNeW22@users.noreply.github.com>
2023-08-15 17:36:06 +0800

    Add files via upload

---
+++ Pointer.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+
+//指针用来存放地址的
+//指针的大小是相同的
+//指针需要多大空间，取决于地址的储存需要多大空间
+{/*
+	printf("%d\n", sizeof(long long));
+
+	printf("%d\n", sizeof (short));
+	printf("%d\n", sizeof (int));
+	printf("%d\n", sizeof (float));
+	printf("%d\n", sizeof (double));
+	printf("%d\n", sizeof(char));*/
+
+
+
+	printf("%d\n", sizeof(long long*));
+
+	printf("%d\n", sizeof(short*));
+	printf("%d\n", sizeof(int*));
+	printf("%d\n", sizeof(float*));
+	printf("%d\n", sizeof(double*));
+	printf("%d\n", sizeof(char*));
+
+
+
+	return 0;
+
+
+}
